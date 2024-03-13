60a0e85ac7a0c112b2e518ab6f7f1ad2a1f93162
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2024-03-13 15:07:57 +0500

    13-mart 15:07

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-1&2y$s@j3@r2gp-jj9qa(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*')
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*')]
 
 # Application definition
 
