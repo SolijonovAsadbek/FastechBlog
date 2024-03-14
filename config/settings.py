397bdcd2957e0f6b7a80cf2d95cff301dee5246b
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2024-03-14 10:53:45 +0500

    14-mart 10:53

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 STATICFILES_DIRS = [BASE_DIR / 'blog_app/static']
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
